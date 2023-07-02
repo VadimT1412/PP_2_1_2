6d8a64686684aee938d32c53505f3b54664b9a2a
Вадим <124818150+VadimT1412@users.noreply.github.com>
2023-07-03 00:44:31 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanOne =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanOne.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+        System.out.println(beanOne == beanTwo);
+        Cat catOne =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catOne.getValue());
+        Cat catTwo =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catTwo.getValue());
+        System.out.println(catOne == catTwo);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,12 +3,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class AppConfig {
- 
+public class  AppConfig {
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setValue("Meow-meow");
+        return cat;
     }
 }
